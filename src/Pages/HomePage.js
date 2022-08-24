5b8aeefd06b0a9751b4e322d4c53eee170566a96
Marcio Bahia <marciosellabahia@hotmail.com>
2022-08-24 19:31:04 -0300

    edit HomePage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -13,20 +13,20 @@ function HomePage() {
                 <Particle />
             </div>
             <div className="typography">
-                <h1>Olá! Me chamo <span>Márcio Bahia</span></h1>
+                <h1>Olá! Me chamo <span>Márcio Bahia</span></h1><br></br>
                 <p>Sou Graduado em Análise e Desenvolvimento de Sistemas pela Universidade Estácio de Sá, 
                 desenvolvo Web Sites modernos e responsivos, aplicativos nativos Android e IOS com as tecnologias React.js no Frontend, 
-                React Native no Mobile e Node.js no Backend:
-                <p><br></br>
+                React Native no Mobile e Node.js no Backend.
+                <p>
 <img src="https://www.luiztools.com.br/wp-content/uploads/2020/06/reactJS.png" height="60px"/>
 &nbsp;  
  <img src="https://www.theconsolelogs.com/react/react-native.png" height="60px"/>
 &nbsp;
 <img src="https://miro.medium.com/max/1131/0*JH_HgIXBQvuenmUl.jpg" height="60px"/>  
 &nbsp;
-            </p><br></br>
             </p>
-            <p>Também desenvolvo em Python, Java e PHP:<br></br>
+            </p>
+            <p>Também desenvolvo em Python, Java e PHP
             <p>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Python_logo_and_wordmark.svg/1280px-Python_logo_and_wordmark.svg.png" height="35px"/>
 &nbsp;  
@@ -35,10 +35,7 @@ function HomePage() {
                 <img src="https://assets.zabbix.com/img/brands/php.svg" height="35px"/>  
 &nbsp;  
         </p>
-            <p>
-                    <br>
-                    </br>
-                   Entre em Contato e me deixe criar uma solução para seu problema de forma rápida e intuitiva, utilizando as mais modernas técnicas e tecnologias de Desenvolvimento de Software.
+            <p>Entre em Contato e me deixe criar uma solução para seu problema de forma rápida e intuitiva, utilizando as mais modernas técnicas e tecnologias de Desenvolvimento de Software.
                     
             </p>    
             </p>
