4ec329b2ec363dfff8a7558a03b9dce9e0fb591d
Marcio Bahia <marciosellabahia@hotmail.com>
2022-08-24 21:03:00 -0300

    Edit HomePage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -15,29 +15,15 @@ function HomePage() {
             <div className="typography">
                 <h1>Olá! Me chamo <span>Márcio Bahia</span></h1><br></br>
                 <p>Sou Graduado em Análise e Desenvolvimento de Sistemas pela Universidade Estácio de Sá, 
-                desenvolvo Web Sites modernos e responsivos, aplicativos nativos Android e IOS com as tecnologias React.js no Frontend, 
+                desenvolvo Web Sites modernos e responsivos, aplicativos nativos Android e IOS com os FrameWorks de JavaScript React.js no Frontend, 
                 React Native no Mobile e Node.js no Backend.
-                <p>
-<img src="https://www.luiztools.com.br/wp-content/uploads/2020/06/reactJS.png" height="60px"/>
-&nbsp;  
- <img src="https://www.theconsolelogs.com/react/react-native.png" height="60px"/>
-&nbsp;
-<img src="https://miro.medium.com/max/1131/0*JH_HgIXBQvuenmUl.jpg" height="60px"/>  
-&nbsp;
             </p>
-            </p>
-            <p>Também desenvolvo em Python, Java e PHP
-            <p>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Python_logo_and_wordmark.svg/1280px-Python_logo_and_wordmark.svg.png" height="35px"/>
-&nbsp;  
-                 <img src="https://dev.java/assets/images/java-logo-vert-blk.png" height="65px"/>
-&nbsp;
-                <img src="https://assets.zabbix.com/img/brands/php.svg" height="35px"/>  
-&nbsp;  
-        </p>
-            <p>Entre em Contato e me deixe criar uma solução para seu problema de forma rápida e intuitiva, utilizando as mais modernas técnicas e tecnologias de Desenvolvimento de Software.
-                    
-            </p>    
+            <br></br>
+            <p>Também desenvolvo em Python, Java, PHP e C, domino as técnicas de Web Designer, 
+                Computação em Nuvem e Web Services, Big Data em Python, Padrões de Projeto de Software com Java, Cloud, IOT e Indústria 4.0.    
+            </p> 
+            <br></br> 
+            <p>Entre em Contato e me deixe criar uma solução para seu problema de forma rápida e intuitiva, utilizando as mais modernas técnicas e tecnologias de Desenvolvimento de Software.  
             </p>
                 <div className="icons">
                     <a href="https://www.facebook.com/marcio.sellabahia/" className="icon i-facebook">
